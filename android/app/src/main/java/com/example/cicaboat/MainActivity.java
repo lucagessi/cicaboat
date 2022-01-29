3f821ee699f6775b13cbb708648fe803a0e882ed
Luca Gessi <luca.gessi90@gmail.com>
2022-01-29 16:47:41 +0100

    First attempt to get phone state events change

--- android/app/src/main/java/com/example/cicaboat/MainActivity.java
+++ android/app/src/main/java/com/example/cicaboat/MainActivity.java
@@ -1,6 +1,26 @@
 package com.example.cicaboat;
 
+import android.os.Bundle;
+import android.util.Log;
 import io.flutter.embedding.android.FlutterActivity;
+import android.content.Context;
+
+//import android.telephony.TelephonyManager;
+//import android.telephony.TelephonyCallback;
+import android.content.Context;
+import android.telephony.PhoneStateListener;
+import java.util.concurrent.Executor;
+//import com.example.cicaboat.ServiceReceiver;
+import android.content.Intent;
 
 public class MainActivity extends FlutterActivity {
+
+    private static final String TAG = "FlutterActivity";
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState); 
+        Log.w(TAG,"Activity created");
+        //phoneStateListener = new CustomPhoneStateListener();
+        startService(new Intent(this, ServiceReceiver.class));
+    }    
 }
--- android/app/src/main/kotlin/com/example/cicaboat/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.cicaboat
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
