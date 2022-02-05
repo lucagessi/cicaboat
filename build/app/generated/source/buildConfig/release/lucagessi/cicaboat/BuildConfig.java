392864b33a42231e791e0329b2e42c4ec0ad8299
Luca Gessi <luca.gessi90@gmail.com>
2022-02-05 17:24:08 +0100

    Buiging serial library

---
+++ build/app/generated/source/buildConfig/release/lucagessi/cicaboat/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package lucagessi.cicaboat;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "lucagessi.cicaboat";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+}
